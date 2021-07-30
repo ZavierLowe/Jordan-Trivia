221e210a8c33dd64203f774ee27e1073b7f57bf0
zavier <zavier.lowe@gmail.com>
2021-07-30 12:02:32 -0400

    Added 2 questions in an array with answers to create some functionality

--- script.js
+++ script.js
@@ -2,7 +2,27 @@
 
 let question  = document.querySelector("#question")
 let questnumb = document.querySelector("#questnumb")
-let questions = ["Which was the First Air Jordan to debut?","Which Jordan sneaker was banned in the NBA?"]
+// Create an array of questions and answers
+const questions = [
+      {question:"Which was the First Air Jordan to debut?",
+      answers:{
+            a: "Air Jordan 1 Chicago",
+            b: "Air Jordan 1 BANNED",
+            c: "Air Jordan 1 Chicago",
+            d: "Air Jordan 1 Chicago"
+      },
+correctAnswer: "a"
+}
+      {question:"Which Jordan sneaker was banned in the NBA?",
+      answers:{
+            a: "Air Jordan 1 BANNED",
+            b: "Air Jordan 1 Chicago",
+            c: "Air Jordan 1 UNC",
+            d: "Air Jordan 1 Chicago"
+      },
+correctAnswer: "b"
+}
+
 let images = document.querySelector("#imgbox")
 question.innerHTML = "Which was the first Air Jordan to Debut"
 questnumb.innerHTML = "Question 1"
@@ -31,5 +51,6 @@ images.addEventListener("click",function(){
 
 
 // Have the questions and the answers in an array
+// Put each answer under the pictures so the Javascript can keep track of them
 // Add an event listener on click to proceed to the next question
 // Need to create a function to proceed to the next question 
