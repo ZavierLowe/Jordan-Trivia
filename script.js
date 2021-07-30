321331fbe1373163884bb4706c2188dec149ff72
zavier <zavier.lowe@gmail.com>
2021-07-30 10:30:36 -0400

    Created dom manipulation for questions of the HTML text

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Create variables and get them by their id for DOM manipulation
 
 let question  = document.querySelector("#question")
+let questnumb = document.querySelector("#questnumb")
 
 
 question.innerHTML = "Which was the first Air Jordan to Debut"
-
+questnumb.innerHTML = "Question 1"
