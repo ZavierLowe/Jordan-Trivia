a597c456e8a8efc3c7e847e9d062d8b6859c783f
zavier <zavier.lowe@gmail.com>
2021-07-30 11:48:01 -0400

    Working on adding javascript questions and answers in an array

--- script.js
+++ script.js
@@ -2,7 +2,34 @@
 
 let question  = document.querySelector("#question")
 let questnumb = document.querySelector("#questnumb")
-
-
+let questions = ["Which was the First Air Jordan to debut?","Which Jordan sneaker was banned in the NBA?"]
+let images = document.querySelector("#imgbox")
 question.innerHTML = "Which was the first Air Jordan to Debut"
 questnumb.innerHTML = "Question 1"
+
+// for (i = 0; questions.length;i++){
+
+// }
+
+images.addEventListener("click",function(){
+
+      for( i = 0; questions.length;i++){
+            if
+      }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Have the questions and the answers in an array
+// Add an event listener on click to proceed to the next question
+// Need to create a function to proceed to the next question 
