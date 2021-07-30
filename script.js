bc14513abe8f80f4a6d5c4ccdafe326ad8763a70
zavier <zavier.lowe@gmail.com>
2021-07-30 10:08:03 -0400

    Addex the boxes at the bottom for the pictures and added some javascript to manipulate the HTML for the questions

---
+++ script.js
@@ -0,0 +1,5 @@
+// Create variables and get them by their id for DOM manipulation
+
+let question  = document.querySelector("#question")
+
+question.innerHTML = "Which was the first Air Jordan to Debut"
